aa6bd959e290a9857f4db5a0c64b0b76245eed55
Shrimad-Bhagwat <sybbhagwat010102@gmail.com>
2021-10-15 10:48:40 +0530

    whitenoise middleware

--- todo/settings.py
+++ todo/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,6 +121,7 @@ LOGIN_URL = 'login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Default primary key field type
@@ -130,7 +132,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #     os.path.join(BASE_DIR,)
 # ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Extra places for collectstatic to find static files.
